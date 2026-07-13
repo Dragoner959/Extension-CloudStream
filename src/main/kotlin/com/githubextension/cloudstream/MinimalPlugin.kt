b9d78bd2b0ef786c85ddc1e8f7b3d665f4feaa72
Dragoner959 <dragonssnchez@gmail.com>
2026-07-12 23:07:12 -0600

    Update MinimalPlugin.kt

--- src/main/kotlin/com/githubextension/cloudstream/MinimalPlugin.kt
+++ src/main/kotlin/com/githubextension/cloudstream/MinimalPlugin.kt
@@ -3,13 +3,14 @@ package com.githubextension.cloudstream
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.TvType
 
 class EmptyAPI : MainAPI() {
     override var mainUrl = "https://example.com"
     override var name = "Empty API"
     override var lang = "en"
     override val hasMainPage = true
-    override val supportedTypes = setOf(com.lagradost.cloudstream3.TvType.Movie)
+    override val supportedTypes = setOf(TvType.Movie)
 }
 
 @CloudstreamPlugin
